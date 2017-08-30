00be6445fb494671d0c57d84b7ecb9d864a37184
Rishats <sultanov-rishat@inbox.ru>
2017-08-30 15:50:36 +0600

    Write simple form without style

--- application/views/login.php
+++ application/views/login.php
@@ -15,8 +15,34 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <title>LOGIN</title>
 </head>
 <body>
-
-test
-
+<form class="center-form">
+    <div class="form-group">
+        <label for="exampleInputEmail1">{{ email }}</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" :placeholder="warning">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">{{ password }}</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" :placeholder="password">
+    </div>
+    <div class="form-check">
+        <label class="form-check-label">
+            <input type="checkbox" class="form-check-input">
+            {{ check }}
+        </label>
+    </div>
+    <button type="submit" class="btn btn-primary">{{ submit }}</button>
+</form>
+<script>
+    new Vue({
+        el: '.center-form',
+        data: {
+            email: "Ваш код",
+            warning: "Укажите секретный код который Вам выдали.",
+            password: "Пароль",
+            check: "Проверить",
+            submit: "Подтвердить"
+        }
+    })
+</script>
 </body>
 </html>
