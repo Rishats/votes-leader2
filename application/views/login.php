6f512d1d835f6a8941a7b059782660cb6a1c8be6
Rishats <sultanov-rishat@inbox.ru>
2017-08-30 13:36:40 +0600

    First data

---
+++ application/views/login.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+<!--    //////////// JS ////////////////////////-->
+    <script src="assets/js/vue.js"></script>
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/tether.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+<!--    /////////// CSS //////////////////////////////////-->
+    <link rel="stylesheet" href="assets/css/tether.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <title>LOGIN</title>
+</head>
+<body>
+
+test
+
+</body>
+</html>
