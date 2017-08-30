04fb62abb6cfa3e09619da592d634e2aa4872ad2
Rishats <sultanov-rishat@inbox.ru>
2017-08-30 23:02:36 +0600

    Some make up

--- application/views/login.php
+++ application/views/login.php
@@ -13,25 +13,37 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <link rel="stylesheet" href="assets/css/tether.css">
     <link rel="stylesheet" href="assets/css/bootstrap.css">
     <title>LOGIN</title>
+    <style>
+        .main-body
+        {
+            background-color: #8193ff;
+        }
+        .main
+        {
+            background-color: #8193ff;
+        }
+        .center-form
+        {
+            background-color: #3b7cff;
+            margin: 300px;
+            border-radius: 20px;
+        }
+    </style>
 </head>
-<body>
+<body class="main-body">
+<div class="main">
 <form class="center-form">
     <div class="form-group">
-        <label for="exampleInputEmail1">{{ email }}</label>
+        <label for="exampleInputEmail1" style="margin-left: 10px">{{ email }}</label>
         <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" :placeholder="warning">
     </div>
     <div class="form-group">
-        <label for="exampleInputPassword1">{{ password }}</label>
+        <label for="exampleInputPassword1" style="margin-left: 10px">{{ password }}</label>
         <input type="password" class="form-control" id="exampleInputPassword1" :placeholder="password">
     </div>
-    <div class="form-check">
-        <label class="form-check-label">
-            <input type="checkbox" class="form-check-input">
-            {{ check }}
-        </label>
-    </div>
-    <button type="submit" class="btn btn-primary">{{ submit }}</button>
+    <button type="submit" class="btn btn-primary" style="margin-left: 40%; margin-bottom: 8px">{{ submit }}</button>
 </form>
+</div>
 <script>
     new Vue({
         el: '.center-form',
@@ -39,7 +51,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             email: "Ваш код",
             warning: "Укажите секретный код который Вам выдали.",
             password: "Пароль",
-            check: "Проверить",
             submit: "Подтвердить"
         }
     })
