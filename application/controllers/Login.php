11044eab03648aa6e00b4bab4de3e4fac25907a7
Rishats <sultanov-rishat@inbox.ru>
2017-09-09 16:04:07 +0600

    Example send data to Email. Without protected and preg_match.

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -6,33 +6,56 @@ class Login extends CI_Controller
         $config['global_xss_filtering'] = TRUE;
         $this->load->view('login');
     }
-    public function register()
+    public function checkEmail()
     {
         $email = $_POST['email'];
         if (preg_match("/[^\d]@iuca.kg$/", $email)){
-            $to  = $email;
-
-            $subject = "Голосуй за лидера!";
-
-            $message = ' 
-        <html> 
-            <head> 
-                <title>Голосуй за лидера!</title> 
-            </head> 
-            <body> 
-                <p>Ваш уникальный код для голосования</p> 
-            </body> 
-        </html>';
-
-            $headers  = "Content-type: text/html; charset=windows-1251 \r\n";
-            $headers .= "From: VOTES LEADER <votes@iuca.kg>\r\n";
-
-
-            mail($to, $subject, $message, $headers);
+            return true;
         }
         else{
-            $this->load->view('login');
+            return false;
+        }
+
+    }
+    public function SendEmail()
+    {
+        $email = $_POST['email'];
+        $to  = $email;
+
+        $subject = "Голосуй за лидера!";
+
+        $message = ' 
+    <html> 
+        <head> 
+            <title>Голосуй за лидера!</title> 
+        </head> 
+        <body> 
+            <p>Ваш уникальный код для голосования</p> 
+        </body> 
+    </html>';
+
+        $headers  = "Content-type: text/html; charset=windows-1251 \r\n";
+        $headers .= "From: VOTES LEADER <votes@iuca.kg>\r\n";
+
+
+        mail($to, $subject, $message, $headers);
+        return true;
+    }
+    public function register()
+    {
+        if($this->checkEmail()){
+            // Send Email With Code.
+            $this->SendEmail();
+            
+            redirect('/auth');
+
+
+        }
+        else{
+            // Email Empty redirect back
+            redirect('/');
         }
 
         }
+
 }
